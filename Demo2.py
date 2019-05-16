bd7f6be1cd621dc53069fc97597670f1f24fbff1
shivaconceptsolution <50239824+shivaconceptsolution@users.noreply.github.com>
2019-05-16 12:40:21 +0530

    Add files via upload

---
+++ Demo2.py
@@ -0,0 +1,5 @@
+a=input("enter first number")
+b=input("enter second number")
+c=int(a) + int(b)  #int() is used to convert string data to integer data
+print("result is "+str(c)) #str() is used to convert integer data to string data
+
---
+++ Demo3.py
@@ -0,0 +1,8 @@
+p = float(input("enter amount"))
+r = float(input("enter rate"))
+t = float(input("enter time"))
+si = (p*r*t)/100
+print("result is "+str(si)) #concatenation  means merge two string type data
+
+
+ 
